8bfe0e6569ad9a990f15d0e1aef2078e51097fe9
Nicolas CAPON <nicolascapon64@gmail.com>
2021-07-27 11:33:50 +0200

    tapping force hold & master right

--- config.h
+++ config.h
@@ -34,17 +34,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+// #define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 #define USE_SERIAL_PD2
 
-#define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
 
 #define NO_ACTION_ONESHOT
-#define TAPPING_FORCE_HOLD
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
